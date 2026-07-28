46e2eede7967a0252ca5d9af03a0b72fea50fcb8
Tarun R <158979401+Ninja10oo@users.noreply.github.com>
2026-07-28 14:16:52 -0500

    Created Java Classes and structuring OOP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/intakeAndTransfer.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Motors;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+
+@Autonomous(name="IntakeAndTransfer", group="Motors")
+public class intakeAndTransfer extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servos/ReleaseServo.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Servos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+
+@Autonomous(name="ReleaseServo", group="Servos")
+public class ReleaseServo extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
