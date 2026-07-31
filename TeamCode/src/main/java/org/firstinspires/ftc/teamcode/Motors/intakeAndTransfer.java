ffbf3ae0f43092741592ef0a2d9be1c1179b998f
Tarun R <158979401+Ninja10oo@users.noreply.github.com>
2026-07-30 20:56:32 -0500

    Update intakeAndTransfer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/intakeAndTransfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/intakeAndTransfer.java
@@ -1,20 +1,20 @@
 package org.firstinspires.ftc.teamcode.Motors;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-
-
-@Autonomous(name="IntakeAndTransfer", group="Motors")
-public class intakeAndTransfer extends OpMode {
-
-    @Override
-    public void init() {
-
+public class intakeAndTransfer {
+    public intakeAndTransfer(HardwareMap hardwareMap, String name) {
+        DcMotorEx Transfer = hardwareMap.get(DcMotorEx.class, name);
+        DcMotorEx intake = hardwareMap.get(DcMotorEx.class, name);
+        intake.setDirection(DcMotor.Direction.FORWARD);
+        Transfer.setDirection(DcMotor.Direction.REVERSE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Transfer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Transfer.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setPower(1.0);
+        Transfer.setPower(1.0);
     }
 
-    @Override
-    public void loop() {
-
-    }
 }
